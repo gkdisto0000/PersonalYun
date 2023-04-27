b12463186066b1f2eed73259c9728aa8779353e8
gkdisto0000 <qufqlcto@naver.com>
2023-04-27 17:25:52 +0900

    230427

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,6 @@
 			}, 100);
 		});
 
-	// Scrolly.
-		$('.scrolly').scrolly();
-
   
   // one slide
   var ag = $('.artGroup');
@@ -185,5 +182,45 @@
       });
     }
   });
+
+
+  //gnb
+		// Hide Header on on scroll down
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 100;
+		var navbarHeight = $('header').outerHeight();
+
+		$(window).scroll(function(event){
+			didScroll = true;
+		});
+
+		setInterval(function() {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, 100);
+
+		function hasScrolled() {
+			var st = $(this).scrollTop();
+						if(Math.abs(lastScrollTop - st) <= delta)
+				return;
+
+			if (st > lastScrollTop && st > navbarHeight){
+				// Scroll Down
+				  $('.gnb').css('top', '0px');
+
+			} else {
+				// Scroll Up
+				if(st + $(window).height() < $(document).height()) {
+				  $('.gnb').css('top', '-80px');
+ 
+				}
+			}
+			lastScrollTop = st;
+		}
+
+
 })(jQuery);
 
